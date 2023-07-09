97623adfbd295a7ac695554df3a44bd1633129a9
kakeru2025 <2019.saki.keru.2023@gmail.com>
2023-07-09 15:50:38 +0900

    コメントとりあえず完成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,7 +19,7 @@ class PostController extends Controller
 
     public function show(Post $post, Comment $comment)
     {
-        return view('posts/show')->with(['post' => $post, 'comment' => $comment->get()]);
+        return view('posts/show')->with(['post' => $post, 'comment' => $post->comments()->get()]);
     }
 
     public function create(Category $category)
